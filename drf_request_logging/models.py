aeedff2b8cef841fae813de018bd89bd52e4d856
OdinsPlasmaRifle <generalaetius.jbvb@gmail.com>
2020-01-24 15:49:29 +0200

    Added new migrations for indexes

--- drf_request_logging/models.py
+++ drf_request_logging/models.py
@@ -7,7 +7,7 @@ from django.conf import settings
 USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 class Request(models.Model):
-    key = models.CharField(null=True, max_length=100)
+    key = models.CharField(db_index=True, null=True, max_length=100)
     user = models.ForeignKey(
         USER_MODEL,
         null=True,
@@ -18,16 +18,18 @@ class Request(models.Model):
         related_name='drf_requests'
     )
     scheme = models.CharField(max_length=5)
-    path = models.CharField(null=True, blank=True, max_length=100)
-    method = models.CharField(max_length=10)
+    path = models.CharField(
+        db_index=True, null=True, blank=True, max_length=100
+    )
+    method = models.CharField(db_index=True, max_length=10)
     encoding = models.CharField(null=True, blank=True, max_length=100)
     content_type = models.CharField(max_length=100)
     params = JSONField(null=True, blank=True, default=dict)
     headers = JSONField(null=True, blank=True, default=dict)
     body = JSONField(null=True, blank=True, default=dict)
-    status_code = models.IntegerField(null=True, blank=True)
+    status_code = models.IntegerField(db_index=True, null=True, blank=True)
     response = models.BinaryField(null=True, blank=True)
-    updated = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True, db_index=True)
     created = models.DateTimeField(auto_now_add=True, db_index=True)
 
     class Meta:
